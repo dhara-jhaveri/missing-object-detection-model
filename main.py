348cd6fbeb406c27bc2d420a0276a8ab05645c06
dhara-jhaveri <jhaveridhara13@gmail.com>
2025-06-16 11:54:20 +0530

    fix: update alert sound file path to include extension

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ model.eval()
 # Configuration
 TARGET_CLASS = 'bottle'
 MISSING_THRESHOLD = 3  # seconds
-ALERT_SOUND = 'alert' 
+ALERT_SOUND = 'alert.mp3' 
 CONFIDENCE_THRESHOLD = 0.3
 
 # COCO class names
